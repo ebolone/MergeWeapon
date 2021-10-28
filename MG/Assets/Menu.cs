b10a80264374151763c17e153536c6b6804fc386
ebolone <92815747+ebolone@users.noreply.github.com>
2021-10-28 17:08:04 +0200

    menus

--- MG/Assets/Menu.cs
+++ MG/Assets/Menu.cs
@@ -5,14 +5,17 @@ using UnityEngine;
 public class Menu : MonoBehaviour
 {
     public string menuName;
-    void open()
+    public bool aperto;
+    public void Open()
     {
+        aperto = true;
         gameObject.SetActive(true);
     }
 
     // Update is called once per frame
-    void close()
+    public void Close()
     {
+        aperto = false;
         gameObject.SetActive(false);
     }
 }
--- MG/Assets/MenuManager.cs
+++ MG/Assets/MenuManager.cs
@@ -4,6 +4,46 @@ using UnityEngine;
 
 public class MenuManager : MonoBehaviour
 {
-    [SerializeField] Menu[] menus;
+    public static MenuManager Instance;
 
+    [SerializeField] Menu[] menus;
+  
+    public void Awake()
+    {
+        Instance = this;
+    }
+    public void OpenMenu(Menu menu)
+    {
+        for (int i = 0; i < menus.Length; i++)
+        {
+
+            if (menus[i].aperto)
+            {
+                CloseMenu(menus[i]);
+            }
+            
+
+        }
+        menu.Open();
+    }
+    public void CloseMenu(Menu menu)
+    {
+        menu.Close();
+    }
+    public void OpenMenu(string nomeMenu)
+    {
+        for(int i = 0; i<menus.Length; i++)
+        {
+            if(menus[i].menuName == nomeMenu)
+            {
+                OpenMenu(menus[i]);
+            }
+            else if (menus[i].aperto)
+            {
+                CloseMenu(menus[i]);
+            }
+
+
+        }
+    }
 }
