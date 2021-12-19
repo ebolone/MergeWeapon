628dbb0a20cd2aa1697c62cb24187a88383355da
LorenzoF47 <lor.furfaro@stud.uniroma3.it>
2021-12-19 11:26:14 +0100

    merge

---
+++ MG/Assets/scomparsa.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scomparsa : MonoBehaviour
+{
+    variabile invisible;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        invisible=GameObject.FindWithTag("Soldato").GetComponent<variabile>();
+        invisible.rend.enabled=true;
+    }
+
+    void OnTriggerEnter()
+    {
+        invisible.rend.enabled=false;
+    }
+
+    void OnTriggerExit()
+    {
+        invisible.rend.enabled=true;
+    }
+    
+}
