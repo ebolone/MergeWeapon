fa62ea39301cf3a7e1fc32b7daaaff672d85665a
ebolone <92815747+ebolone@users.noreply.github.com>
2022-01-05 12:49:15 +0100

    update3-weapon-sync

--- MG/Assets/Scripts/WeaponChoosing.cs
+++ MG/Assets/Scripts/WeaponChoosing.cs
@@ -1,15 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
+using Hashtable = ExitGames.Client.Photon.Hashtable;
+using Photon.Realtime;
 
-public class WeaponChoosing : MonoBehaviour
+public class WeaponChoosing : MonoBehaviourPunCallbacks
 {
     public static int selectedArma1;
     public static int selectedArma2;
+    [SerializeField] PhotonView player_PV;
     // Start is called before the first frame update
     void Start()
     {
-        selectWeapon();
+        if (player_PV.IsMine)
+        {
+            SelectWeapon(selectedArma1, selectedArma2);
+            Hashtable hash1 = new Hashtable();
+            hash1.Add("item index1", selectedArma1);
+            hash1.Add("item index2", selectedArma2);
+            PhotonNetwork.LocalPlayer.SetCustomProperties(hash1);
+        }
+         
         
     }
 
@@ -18,14 +30,19 @@ public class WeaponChoosing : MonoBehaviour
     {
         
     }
-    void selectWeapon()
+    void SelectWeapon(int index1, int index2)
     {
         int i = 0;
         foreach(Transform weapon in transform)
         {
-            if (i == selectedArma1 || i == selectedArma2)
+            if (i == index1 || i == index2)
                 weapon.gameObject.SetActive(true);
             i++;
         }
+    }
+    public override void OnPlayerPropertiesUpdate(Player targetPlayer, Hashtable changedProps)
+    {
+        if (!player_PV.IsMine && targetPlayer == player_PV.Owner)
+            SelectWeapon((int)changedProps["item index1"], (int)changedProps["item index2"]);
     }
 }
