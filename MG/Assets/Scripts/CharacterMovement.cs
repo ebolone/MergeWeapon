031418b3219433a67ce7eb8bf41f725f0eb9e9bc
ebolone <92815747+ebolone@users.noreply.github.com>
2022-02-16 18:50:06 +0100

    charachter-move-update

--- MG/Assets/Scripts/CharacterMovement.cs
+++ MG/Assets/Scripts/CharacterMovement.cs
@@ -2,15 +2,18 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 using System.Collections;
 using System.Collections.Generic;
+using Photon.Pun;
 
 
 
-public class CharacterMovement : MonoBehaviour
+public class CharacterMovement : MonoBehaviourPunCallbacks
 {
     public Transform firePoint;
     public GameObject bulletPrefab;
     float fireRate = 0.3f;
     float timePassed = 0f;
+
+    PhotonView view;
 
 
     private CharacterController controller;
@@ -29,49 +32,61 @@ public class CharacterMovement : MonoBehaviour
         controller = gameObject.AddComponent<CharacterController>();
         animator = gameObject.GetComponent<Animator>();
         playerInput = gameObject.GetComponent<PlayerInput>();
+        view = GetComponent<PhotonView>();
+
     }
 
     void Update()
     { 
-
-        Vector2 joystickInput = playerInput.actions["Move"].ReadValue<Vector2>();
-        Vector3 direction = new Vector3 (joystickInput.x, 0, joystickInput.y).normalized;
-        
-        Vector2 joystickInputt = playerInput.actions["Look"].ReadValue<Vector2>();
-        Vector3 joystickLook = new Vector3 (joystickInputt.x, 0, joystickInputt.y).normalized;
-        if(joystickLook!= Vector3.zero)
+        if(photonView.IsMine == false && PhotonNetwork.IsConnected == true)
         {
-            gg = joystickLook;
+            return;
         }
+        if (view.IsMine)
+        {
 
-        bool shot = playerInput.actions["Shoot"].triggered;
-        bool shot2 = playerInput.actions["Shoot2"].triggered;
+            Vector2 joystickInput = playerInput.actions["Move"].ReadValue<Vector2>();
+            Vector3 direction = new Vector3(joystickInput.x, 0, joystickInput.y).normalized;
 
-        if (shot || shot2)
-        {
-            direction = Vector3.zero;
-            gameObject.transform.forward=gg;
-        } else if(direction != Vector3.zero) 
-        {
-            //gameObject.transform.forward=direction;
-            animator.SetBool("isWalking", true);
-        } else if(joystickLook != Vector3.zero)
-        {
-           gameObject.transform.forward=joystickLook;
-        } else
-        {
-            animator.SetBool("isWalking", false);
-        }
+            Vector2 joystickInputt = playerInput.actions["Look"].ReadValue<Vector2>();
+            Vector3 joystickLook = new Vector3(joystickInputt.x, 0, joystickInputt.y).normalized;
+            if (joystickLook != Vector3.zero)
+            {
+                gg = joystickLook;
+            }
 
-        if(direction.magnitude >= 0.1f)
-        {
-            
-            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
-            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
-            transform.rotation = Quaternion.Euler (0f, angle, 0f);
+            bool shot = playerInput.actions["Shoot"].triggered;
+            bool shot2 = playerInput.actions["Shoot2"].triggered;
 
-            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
-            controller.Move(direction * playerSpeed * Time.deltaTime);
+            if (shot || shot2)
+            {
+                direction = Vector3.zero;
+                gameObject.transform.forward = gg;
+            }
+            else if (direction != Vector3.zero)
+            {
+                //gameObject.transform.forward=direction;
+                animator.SetBool("isWalking", true);
+            }
+            else if (joystickLook != Vector3.zero)
+            {
+                gameObject.transform.forward = joystickLook;
+            }
+            else
+            {
+                animator.SetBool("isWalking", false);
+            }
+
+            if (direction.magnitude >= 0.1f)
+            {
+
+                float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
+                float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+                transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+                Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+                controller.Move(direction * playerSpeed * Time.deltaTime);
+            }
         }
     }
 }
