8049d15b13991f40e49356843fef2fa8528e10f5
ebolone <92815747+ebolone@users.noreply.github.com>
2022-02-16 12:25:59 +0100

    bloom-shooting&hit-effect

--- MG/Assets/ProjectileMove.cs
+++ MG/Assets/ProjectileMove.cs
@@ -8,23 +8,36 @@ public class ProjectileMove : MonoBehaviour
     public float fireRate;
     public int  numeroColpi = 1;
     public float tempoTraColpiConsecutivi = 0;
+    public GameObject hitPrefab;
+    public float maxSpread = 0;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Vector3 dir = transform.forward + new Vector3(Random.Range(-maxSpread, maxSpread), Random.Range(-maxSpread, maxSpread), Random.Range(-maxSpread, maxSpread));
+        this.GetComponent<Rigidbody>().AddForce(dir * speed);
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (speed != 0)
-            transform.position += transform.forward * (speed * Time.deltaTime);
-        else Debug.Log("no speed");
+        //if (speed != 0)
+            //transform.position += transform.forward * (speed * Time.deltaTime) ; 
+       // else Debug.Log("no speed");
         
     }
     private void OnCollisionEnter(Collision collision)
     {
         speed = 0;
+        ContactPoint contact = collision.contacts[0];
+        Quaternion rot = Quaternion.FromToRotation(Vector3.up, contact.normal);
+        Vector3 pos = contact.point;
+        if(hitPrefab != null)
+        {
+            var hitVFX = Instantiate(hitPrefab, pos, rot);
+
+        }
+        
         Destroy(gameObject);
     }
 }
